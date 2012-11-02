85323275fbbe9450825828dca1d5c4db5045fc62
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2012-11-02 11:31:02 +0000

    Make 'about' page new index

--- controller.rb
+++ controller.rb
@@ -23,35 +23,39 @@ helpers do
   def git_version_number
     `git log | sed -n 1p`.split(" ").last
   end
+
+  def url(page='')
+    "http%3A%2F%2Fwebaudio.prototyping.bbc.co.uk%2F#{page}"
+  end
 end
 
 before 'index.html.erb' do
-  layout 'landing_layout.html.erb'
-  @machine = "index"
-end
-
-before 'about.html.erb' do
   layout 'layout.html.erb'
   @machine = "about"
   @show_prev_next_buttons = false
+  @url = url() # index page 
 end
 
 before 'wobbulator.html.erb' do
   @code = extract_code_for("docs/wobbulator.html")
   @machine = "wobbulator"
+  @url = url("#{@machine}.html")
 end
 
 before 'ring-modulator.html.erb' do
   @code = extract_code_for("docs/ring-modulator.html")
   @machine = "ring-modulator"
+  @url = url("#{@machine}.html")
 end
 
 before 'gunfire.html.erb' do
   @code = extract_code_for("docs/gunfire.html")
   @machine = "gunfire"
+  @url = url("#{@machine}.html")
 end
 
 before 'tapeloops.html.erb' do
   @code = extract_code_for("docs/tapeloops.html")
   @machine = "tapeloops"
+  @url = url("#{@machine}.html")
 end
