f5be76389e564f7cb51efe71d166f17b673a9fcb
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2012-11-06 12:23:50 +0000

    Cleaner URLs (remove '.html')

--- controller.rb
+++ controller.rb
@@ -24,7 +24,7 @@ helpers do
     `git log | sed -n 1p`.split(" ").last
   end
 
-  def url(page='')
+  def encoded_url(page='')
     "http%3A%2F%2Fwebaudio.prototyping.bbc.co.uk%2F#{page}"
   end
 end
@@ -33,29 +33,29 @@ before 'index.html.erb' do
   layout 'layout.html.erb'
   @machine = "about"
   @show_prev_next_buttons = false
-  @url = url() # index page 
+  @encoded_url = encoded_url() # index page 
 end
 
-before 'wobbulator.html.erb' do
+before 'wobbulator/index.html.erb' do
   @code = extract_code_for("docs/wobbulator.html")
   @machine = "wobbulator"
-  @url = url("#{@machine}.html")
+  @encoded_url = encoded_url(@machine)
 end
 
-before 'ring-modulator.html.erb' do
+before 'ring-modulator/index.html.erb' do
   @code = extract_code_for("docs/ring-modulator.html")
   @machine = "ring-modulator"
-  @url = url("#{@machine}.html")
+  @encoded_url = encoded_url(@machine)
 end
 
-before 'gunfire.html.erb' do
+before 'gunfire/index.html.erb' do
   @code = extract_code_for("docs/gunfire.html")
   @machine = "gunfire"
-  @url = url("#{@machine}.html")
+  @encoded_url = encoded_url(@machine)
 end
 
-before 'tapeloops.html.erb' do
+before 'tapeloops/index.html.erb' do
   @code = extract_code_for("docs/tapeloops.html")
   @machine = "tapeloops"
-  @url = url("#{@machine}.html")
+  @encoded_url = encoded_url(@machine)
 end
